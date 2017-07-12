5063babe20273d571068ed5e4ff9bc9fefed9c3a
Robert Puls <rpu1s@sbcglobal.net>
2017-07-12 13:26:50 -0600

    connecting to heroku

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,7 @@ $(() => {
     if (window.location.hostname == "localhost") {
       BASE_URL = `http://localhost:3000`;
     } else {
-      BASE_URL = ``;
+      BASE_URL = `https://galvanize-read-server.herokuapp.com`;
     }
   })();
 
