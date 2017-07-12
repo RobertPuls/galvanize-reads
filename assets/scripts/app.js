e0caaa67d3eeb635aa0a55430edb5225a702f57b
Robert Puls <rpu1s@sbcglobal.net>
2017-07-12 11:28:46 -0600

    puts book data on page

---
+++ assets/scripts/app.js
@@ -0,0 +1,31 @@
+$(() => {
+  let BASE_URL;
+
+  (function getBaseURL() {
+    if (window.location.hostname == "localhost") {
+      BASE_URL = `http://localhost:3000`;
+    } else {
+      BASE_URL = ``;
+    }
+  })();
+
+  $.get(`${BASE_URL}/all`, data => handlebars(data, "#bookList", "#book"));
+
+
+  function handlebars(data, appendTo, sourceFrom) {
+    const source = $(sourceFrom).html();
+    const template = Handlebars.compile(source);
+    for (let i = 0; i < data.length; i++) {
+      console.log(data[i].authors.forEach(author => console.log(author)));
+      const context = {
+        "book_title": data[i].book_name,
+        "book_genre": data[i].book_genre,
+        "book_desc": data[i].book_desc,
+        "book_cover": data[i].book_cover,
+        "authors": data[i].authors
+      };
+      const html = template(context);
+      $(appendTo).append(html);
+    }
+  }
+});
