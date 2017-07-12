3dc0d8b83a969062601dc055741956b892f19309
Robert Puls <rpu1s@sbcglobal.net>
2017-07-12 16:03:37 -0600

    Delete button finished

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,21 +1,28 @@
 $(() => {
-  $.get(`${BASE_URL}/book/all`, data => handlebars(data, "#bookList", "#book"));
-
-
-  function handlebars(data, appendTo, sourceFrom) {
-    const source = $(sourceFrom).html();
-    const template = Handlebars.compile(source);
-    for (let i = 0; i < data.length; i++) {
-      console.log(data[i].authors.forEach(author => console.log(author)));
-      const context = {
-        "book_title": data[i].book_name,
-        "book_genre": data[i].book_genre,
-        "book_desc": data[i].book_desc,
-        "book_cover": data[i].book_cover,
-        "authors": data[i].authors
-      };
-      const html = template(context);
-      $(appendTo).append(html);
-    }
-  }
+  $.get(`${BASE_URL}/book/all`, data => handlebars(data, "#bookList", "#book"))
+  .then(() => {
+    $(".removeBtn").on("click",function() {
+      $.ajax({
+        method: "DELETE",
+        url: `${BASE_URL}/book/${this.value}`,
+      }).done(() => alert("Deleted"));
+    });
+  });
 });
+
+function handlebars(data, appendTo, sourceFrom) {
+  const source = $(sourceFrom).html();
+  const template = Handlebars.compile(source);
+  for (let i = 0; i < data.length; i++) {
+    const context = {
+      "book_id": data[i].book_id,
+      "book_title": data[i].book_name,
+      "book_genre": data[i].book_genre,
+      "book_desc": data[i].book_desc,
+      "book_cover": data[i].book_cover,
+      "authors": data[i].authors
+    };
+    const html = template(context);
+    $(appendTo).append(html);
+  }
+}
